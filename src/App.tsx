91014cc373456413b4080df6e98a2ac8d8d69948
parasharanurag234 <156665673+parasharanurag234@users.noreply.github.com>
2024-06-24 12:20:50 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-// import About from "./page/about/About";
-// import Home from "./page/home/Home";
-// import Projects from "./page/projects/Projects";
-// import Conatct from "./page/contact/Contact";
-import CursorComponent from "./components/cursor/CursorComponent";
-import Layout from "./page/layout/Layout";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   Navigate,
+// } from "react-router-dom";
+
+// import CursorComponent from "./components/cursor/CursorComponent";
+// import Layout from "./page/layout/Layout";
 
 function App() {
   return (
@@ -20,10 +17,7 @@ function App() {
     //       <Route path="*" element={<Navigate to="/layout" replace />} />
     //       <Route path="/" element={<Navigate to="/layout" />} />
     //       <Route path="/layout" element={<Layout />} />
-    //       {/* <Route path="/home" element={<Home />} />
-    //       <Route path="/about" element={<About />} />
-    //       <Route path="/projects" element={<Projects />} />
-    //       <Route path="/contact" element={<Conatct />} /> */}
+
     //     </Routes>
     //   </div>
     // </Router>
