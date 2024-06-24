9f32a16e3f554989c36b6fae1ac2d03091e8ab67
parasharanurag234 <156665673+parasharanurag234@users.noreply.github.com>
2024-06-24 12:18:46 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,20 +13,23 @@ import Layout from "./page/layout/Layout";
 
 function App() {
   return (
-    <Router>
-      <CursorComponent/>
-      <div className="app">
-        <Routes>
-          <Route path="*" element={<Navigate to="/layout" replace />} />
-          <Route path="/" element={<Navigate to="/layout" />} />
-          <Route path="/layout" element={<Layout />} />
-          {/* <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Conatct />} /> */}
-        </Routes>
-      </div>
-    </Router>
+    // <Router>
+    //   <CursorComponent/>
+    //   <div className="app">
+    //     <Routes>
+    //       <Route path="*" element={<Navigate to="/layout" replace />} />
+    //       <Route path="/" element={<Navigate to="/layout" />} />
+    //       <Route path="/layout" element={<Layout />} />
+    //       {/* <Route path="/home" element={<Home />} />
+    //       <Route path="/about" element={<About />} />
+    //       <Route path="/projects" element={<Projects />} />
+    //       <Route path="/contact" element={<Conatct />} /> */}
+    //     </Routes>
+    //   </div>
+    // </Router>
+    <div>
+    App
+    </div>
   );
 }
 
